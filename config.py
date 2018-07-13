2b6d0e5157ff1ffe40d90a095dbcf45f8c782de5
OZOAR <vadimrezunik@yandex.ru>
2018-07-13 11:29:24 +0300

    version 1.0.1.5

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 last_id = None
-token = '607749123:AAE5sGKiqJm6WpO27b0IuP9i38WYAO41UtM'  # '612157386:AAGSHk-mOQMZCgOpN8EGofcyzVX1bna2wlM'
+token = '612157386:AAGSHk-mOQMZCgOpN8EGofcyzVX1bna2wlM'
 image_path = os.path.join(r'C:\Users\OZOAR\Desktop\BotTest\images', 'images')
